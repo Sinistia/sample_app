4836d9213610f189777b4af83fed19ce10373600
Tim van der Ende <toetsentim@live.nl>
2014-06-02 20:08:10 +0200

    Finish User signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Tim van der Ende"
+    email    "toetsentim@live.nl"
+    password "marantz2225"
+    password_confirmation "marantz2225"
+  end
+end
